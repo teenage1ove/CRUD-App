0b3b44836c5edcce2b194630307aa89c7db56516
teenage1ove <kirillstars7@gmail.com>
2024-06-03 16:51:16 +0300

    Add local storage

--- crud-app/src/store/User.ts
+++ crud-app/src/store/User.ts
@@ -1,31 +1,45 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 import { IUserList, userList } from '../data'
 
+const LS_USERS_KEY = 'luk'
 
+interface IUser {
+    name: string,
+    email: string,
+    id: number
+}
+
+const initialState:IUser[] = [
+    ...JSON.parse(localStorage.getItem(LS_USERS_KEY) ?? '[]') 
+]
 
 export const userSlice = createSlice({
     name: 'users',
-    initialState: userList,
+    initialState: initialState,
     reducers: {
-        addUser: (state, action: PayloadAction<IUserList>) => {
+        addUser: (state, action: PayloadAction<IUser>) => {
             state.push(action.payload)
+            localStorage.setItem(LS_USERS_KEY, JSON.stringify(state))
+            console.log()
         },
-        updateUser: (state, action: PayloadAction<IUserList>) => {
+        updateUser: (state, action: PayloadAction<IUser>) => {
             const {id, name, email} = action.payload
             const uu = state.find(user => user.id === id)
             if (uu) {
                 uu.name = name
                 uu.email = email
+                localStorage.setItem(LS_USERS_KEY, JSON.stringify(state))
             }
+            
         },
         deleteUser : (state, action) => {
             const {id} = action.payload
             const uu = state.find(user => user.id === id)
             if (uu) {
-                return state.filter(user => user.id !== id)
+                state = state.filter(user => user.id !== id)
+                localStorage.setItem(LS_USERS_KEY, JSON.stringify(state))
+                return state
             }
-            
-            
         }
     }
 })
